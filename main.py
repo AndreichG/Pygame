82b48d73a04e8784d8abb386dafdf6a6e99921c3
Andrey <andreichg1787@gmail.com>
2025-01-08 19:48:34 +0300

    grid and improved player rotations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from player import Player
+from grid import Grid
 
 
 pygame.init()
@@ -11,7 +12,8 @@ fps = 60
 
 def main():
     all_sprites = pygame.sprite.Group()
-    player = Player(screen, all_sprites)
+    grid = Grid(screen, (128, 128), 64)
+    player = Player(screen, grid, all_sprites)
 
 
     running = True
@@ -20,7 +22,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.VIDEORESIZE:
+                player.sc_resize()
+        #grid.render()
+        #pygame.draw.rect(screen, (255, 0, 0), (screen.get_width() / 5, screen.get_height() / 5, screen.get_width() / 5 * 3, screen.get_height() / 5 * 3), 2)
+        #pygame.draw.rect(screen, (0, 255, 0), (player.rect.x, player.rect.y, player.rect.width, player.rect.height), 2)
+        #pygame.draw.circle(screen, (255, 0, 0), grid.center, 5)
+        #Отладка для игрока и сетки
+
+        player.move()
+        player.check()
         all_sprites.draw(screen)
+        pygame.draw.circle(screen, (0, 0, 0), player.rect.center, 5)
         clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
--- player.py
+++ player.py
@@ -1,7 +1,8 @@
-from math import atan2, sin, cos
 import os
 import sys
+from math import sin,cos, pi
 import pygame
+
 
 def load_image(name):
     fullname = os.path.join('data', name)
@@ -11,14 +12,96 @@ def load_image(name):
     image = pygame.image.load(fullname)
     return image
 
+
 class Player(pygame.sprite.Sprite):
     image = load_image("player1.png")
 
-    def __init__(self, screen, *group):
+    def __init__(self, screen, grid, *group):
         super().__init__(*group)
         self.image = Player.image
         self.rect = self.image.get_rect()
+        self.mask = self.image.get_masks()
+        self.p0 = self.rect.center
+        self.v = 5
+
+        self.screen = screen
+        self.screen_size = (screen.get_width(), screen.get_height())
         self.rect.center = (screen.get_width() / 2, screen.get_height() / 2)
+        self.grid = grid
+        self.g = (0, 0)
+
+    def rotate(self, angle):
+        x, y = self.rect.centerx, self.rect.centery
+        self.image = pygame.transform.rotate(Player.image, angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            if keys[pygame.K_a]:
+                self.rotate(45)
+                self.rect.x += -self.v * cos(pi / 4)
+                self.rect.y += -self.v * sin(pi / 4)
+            elif keys[pygame.K_d]:
+                self.rotate(-45)
+                self.rect.x += self.v * cos(pi / 4)
+                self.rect.y += -self.v * sin(pi / 4)
+            else:
+                self.rotate(0)
+                self.rect.y += -self.v
+
+        elif keys[pygame.K_s]:
+            if keys[pygame.K_a]:
+                self.rotate(135)
+                self.rect.x += -self.v * cos(pi / 4)
+                self.rect.y += self.v * sin(pi / 4)
+            elif keys[pygame.K_d]:
+                self.rotate(-135)
+                self.rect.x += self.v * cos(pi / 4)
+                self.rect.y += self.v * sin(pi / 4)
+            else:
+                self.rotate(180)
+                self.rect.y += self.v
+
+        elif keys[pygame.K_a] and not (keys[pygame.K_w] and keys[pygame.K_s]):
+            self.rotate(90)
+            self.rect.x += -self.v
+
+        elif keys[pygame.K_d] and not (keys[pygame.K_w] and keys[pygame.K_s]):
+            self.rotate(-90)
+            self.rect.x += self.v
+
+
+        self.g = (self.rect.centerx - self.grid.center[0], self.rect.centery - self.grid.center[1])
+
+    def check(self):
+        dx, dy = 0, 0
+        if self.rect.centerx < int(self.screen.get_width() / 5):
+            dx = self.rect.centerx - int(self.screen.get_width() / 5)
+            self.rect.centerx = int(self.screen.get_width() / 5)
+        if self.rect.centerx > int(self.screen.get_width() / 5 * 4):
+            dx = self.rect.centerx - int(self.screen.get_width() / 5 * 4)
+            self.rect.centerx = int(self.screen.get_width() / 5 * 4)
+        if self.rect.centery < int(self.screen.get_height() / 5):
+            dy = self.rect.centery - int(self.screen.get_height() / 5)
+            self.rect.centery = int(self.screen.get_height() / 5)
+        if self.rect.centery > int(self.screen.get_height() / 5 * 4):
+            dy = self.rect.centery - int(self.screen.get_height() / 5 * 4)
+            self.rect.centery = int(self.screen.get_height() / 5 * 4)
+        print(self.rect.centery, int(self.screen.get_height() / 5 * 4))
+        self.grid.move((dx, dy))
+
+    def sc_resize(self):
+        x = self.rect.centerx / self.screen_size[0]
+        y = self.rect.centery / self.screen_size[1]
+        self.rect.centerx = self.screen.get_width() * x
+        self.rect.centery = self.screen.get_height() * y
+        self.screen_size = (self.screen.get_width(), self.screen.get_height())
+        self.grid.center[0] = self.rect.centerx - self.g[0]
+        self.grid.center[1] = self.rect.centery - self.g[1]
+
+
 
 
 
